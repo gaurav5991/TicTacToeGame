d0d53c756d6fcd2d12a691b2ac56cf1a9ad94f2f
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-10-07 14:33:19 +0530

    [Gaurav]ADDED: Check For Winning Condition

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -5,41 +5,52 @@ import java.util.Scanner;
 public class TicTacToeGame {
 
     public static final Scanner sc = new Scanner(System.in);
-    public static char []gameBoard = new char[10];
+    public static char[] gameBoard = new char[10];
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe");
-        creategameBoard();
-
+        createGameBoard();
         System.out.println("Enter the Symbol: ");
         char[] input = userInput();
-
         System.out.println("User input " + input[0]);
         System.out.println("Computer input " + input[1]);
-
-        System.out.println("Enter User Choice Whether its Head Or Tail");
+        System.out.println("Enter User Choice Whether its head/tail");
         String toss = sc.next();
         int toss_result;
         String result = doTossToCheckTurn(toss);
-        if (result.equals("USER"))
+        if (result.equals("Player"))
             toss_result = 1;
         else
             toss_result = 2;
         System.out.println(result + " won the toss");
 
-        System.out.println("Enter index you want to move from 1 to 9");
-        int index = sc.nextInt();
+        displayGameBoard();
+        boolean flag = false;
+        switch (toss_result) {
+            case 1: {
+                while (true) {
+                    System.out.println("Enter the Position of Choice");
+                    int position = sc.nextInt();
 
-        System.out.println("Display gameBoard");
-        makeAMove(index, input[0]);
+                    makeAMove(position, input[0]);
+                    boolean UserResult = winningCondition(input[0]);
+                    if (UserResult == true) {
+                        System.out.println("Player won");
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+
+        }
     }
 
     /*Board Created*/
 
-    public static char[] creategameBoard() {
-        gameBoard[0]=' ';
-        for (int i = 1; i < 10; i++) {
-            gameBoard[i] = ' ';
+    public static char[] createGameBoard() {
+        gameBoard[0] = ' ';
+        for (int position = 1; position < 10; position++) {
+            gameBoard[position] = ' ';
         }
         return gameBoard;
     }
@@ -68,29 +79,26 @@ public class TicTacToeGame {
     }
 
     /* Check Index to Make Move if its Empty*/
-    public static boolean ifIndexEmptyOrNot(int index)
-    {
+    public static boolean ifIndexEmptyOrNot(int index) {
         if (index > 0 && index <= 9 && gameBoard[index] == ' ')
-        {
             return true;
-        }
-        else {
+        else
             return false;
-        }
     }
 
     /*Make A Move */
-    public static void makeAMove(int index,char moveSymbol){
-        if(ifIndexEmptyOrNot(index)){
+    public static void makeAMove(int index, char moveSymbol) {
+        if (ifIndexEmptyOrNot(index)) {
             gameBoard[index] = moveSymbol;
             displayGameBoard();
-        }else {
+        } else {
             System.out.println("The index is Invalid.");
             System.out.println("Enter new Position");
             int newPosition = sc.nextInt();
-            makeAMove(newPosition,moveSymbol);
+            makeAMove(newPosition, moveSymbol);
         }
     }
+
     /* Do A Toss*/
     public static String doTossToCheckTurn(String toss) {
         int flag;
@@ -99,10 +107,19 @@ public class TicTacToeGame {
         else
             flag = 1;
         int tossResult = (int) Math.floor(Math.random() * 10) % 2;
-        if(tossResult==flag){
+        if (tossResult == flag) {
             return "Player";
-        }else {
+        } else {
             return "Computer";
         }
+    }
+
+    /*Condition for Winning*/
+
+    public static boolean winningCondition(char symbol) {
+        return ((gameBoard[1] == symbol && gameBoard[2] == symbol && gameBoard[3] == symbol) || (gameBoard[4] == symbol && gameBoard[5] == symbol && gameBoard[6] == symbol)
+                || (gameBoard[7] == symbol && gameBoard[8] == symbol && gameBoard[9] == symbol) || (gameBoard[1] == symbol && gameBoard[4] == symbol && gameBoard[7] == symbol)
+                || (gameBoard[2] == symbol && gameBoard[5] == symbol && gameBoard[8] == symbol) || (gameBoard[3] == symbol && gameBoard[6] == symbol && gameBoard[9] == symbol)
+                || (gameBoard[1] == symbol && gameBoard[5] == symbol && gameBoard[9] == symbol) || (gameBoard[3] == symbol && gameBoard[5] == symbol && gameBoard[7] == symbol));
     }
 }
