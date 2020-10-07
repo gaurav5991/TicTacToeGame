c651e2dda23fba3ddd22a54c7bedfe794d46bb51
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-10-07 10:42:36 +0530

    [Gaurav]ADDED: Display Game Board

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -12,23 +12,44 @@ public class TicTacToeGame {
         TicTacToeGame game = new TicTacToeGame();
         char[] board = game.creatgameBoard();
 
-        char input = TicTacToeGame.userInput();
-        System.out.println("Player Input: "+input);
+        char []input = userInput();
+        System.out.println("Player Input: "+input[0]);
+        System.out.println("Computer Input: "+input[1]);
 
+        displayGameBoard();
     }
-    public char[] creatgameBoard(){
+
+                /*Board Created*/
+
+    public static char[] creatgameBoard(){
         int gameBoardSize = 10;
         char [] gameBoard = new char[gameBoardSize];
-        for (int i= 0;i<gameBoardSize;i++){
+        for (int i= 1;i<gameBoardSize;i++){
             gameBoard[i] = ' ';
         }
         return gameBoard;
     }
 
-    public static char userInput(){
+    /*User and Computer Input*/
+    public static char[] userInput(){
         System.out.println("User Input : x or o");
-        char symbol = sc.next().charAt(0);
-
+        char []symbol = new char[2];
+        symbol[0] = sc.next().charAt(0);
+        symbol[1] = ' ';
+        if(symbol[0]=='x'){
+            symbol[1]= 'o';
+        }else {
+            symbol[1] = 'x';
+        }
         return  symbol;
+    }
+
+    /*Show GameBoard*/
+    public static  void displayGameBoard(){
+        creatgameBoard();
+        for (int i = 1; i <10; i++) {
+            System.out.println(creatgameBoard()[i++] +" | "+creatgameBoard()[i++]+" | "+creatgameBoard()[i]);
+            System.out.println();
+        }
     }
 }
