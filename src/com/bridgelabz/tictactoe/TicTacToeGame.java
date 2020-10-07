d11a8d1ba46b2057f436aa83890af3151502752f
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-10-07 11:38:03 +0530

    [Gaurav]REFACTORED: Display Game Board

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -10,46 +10,47 @@ public class TicTacToeGame {
         System.out.println("Welcome to Tic Tac Toe");
 
         TicTacToeGame game = new TicTacToeGame();
-        char[] board = game.creatgameBoard();
+        char[] board = creatgameBoard();
 
-        char []input = userInput();
-        System.out.println("Player Input: "+input[0]);
-        System.out.println("Computer Input: "+input[1]);
+        char[] input = userInput();
+        System.out.println("Player Input: " + input[0]);
+        System.out.println("Computer Input: " + input[1]);
 
         displayGameBoard();
     }
 
-                /*Board Created*/
+    /*Board Created*/
 
-    public static char[] creatgameBoard(){
+    public static char[] creatgameBoard() {
         int gameBoardSize = 10;
-        char [] gameBoard = new char[gameBoardSize];
-        for (int i= 1;i<gameBoardSize;i++){
+        char[] gameBoard = new char[gameBoardSize];
+        for (int i = 1; i < gameBoardSize; i++) {
             gameBoard[i] = ' ';
         }
         return gameBoard;
     }
 
     /*User and Computer Input*/
-    public static char[] userInput(){
+    public static char[] userInput() {
         System.out.println("User Input : x or o");
-        char []symbol = new char[2];
+        char[] symbol = new char[2];
         symbol[0] = sc.next().charAt(0);
         symbol[1] = ' ';
-        if(symbol[0]=='x'){
-            symbol[1]= 'o';
-        }else {
+        if (symbol[0] == 'x') {
+            symbol[1] = 'o';
+        } else {
             symbol[1] = 'x';
         }
-        return  symbol;
+        return symbol;
     }
 
     /*Show GameBoard*/
-    public static  void displayGameBoard(){
+    public static void displayGameBoard() {
         creatgameBoard();
-        for (int i = 1; i <10; i++) {
-            System.out.println(creatgameBoard()[i++] +" | "+creatgameBoard()[i++]+" | "+creatgameBoard()[i]);
-            System.out.println();
-        }
+        System.out.println(creatgameBoard()[1] + " | " + creatgameBoard()[2] + " | " + creatgameBoard()[3]);
+        System.out.println("-----------");
+        System.out.println(creatgameBoard()[4] + " | " + creatgameBoard()[5] + " | " + creatgameBoard()[6]);
+        System.out.println("-----------");
+        System.out.println(creatgameBoard()[7] + " | " + creatgameBoard()[8] + " | " + creatgameBoard()[9]);
     }
 }
