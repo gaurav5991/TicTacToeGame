fa4125e466af5d1e5da2acd33f3d835f68e1fb7d
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-10-07 09:55:35 +0530

    [Gaurav]ADDED: User input for Player Symbol

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,9 +1,19 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe");
+
         TicTacToeGame game = new TicTacToeGame();
         char[] board = game.creatgameBoard();
+
+        char input = TicTacToeGame.userInput();
+        System.out.println("Player Input: "+input);
 
     }
     public char[] creatgameBoard(){
@@ -13,5 +23,12 @@ public class TicTacToeGame {
             gameBoard[i] = ' ';
         }
         return gameBoard;
+    }
+
+    public static char userInput(){
+        System.out.println("User Input : x or o");
+        char symbol = sc.next().charAt(0);
+
+        return  symbol;
     }
 }
