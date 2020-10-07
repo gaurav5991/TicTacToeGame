da2a913b16e6ff1f0c0647a48716bb258358709f
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-10-07 12:31:36 +0530

    [Gaurav]ADDED: Check For Free Space on Board

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -8,22 +8,31 @@ public class TicTacToeGame {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe");
-
-        TicTacToeGame game = new TicTacToeGame();
-        char[] board = creatgameBoard();
-
+        System.out.println("Enter the Symbol: ");
         char[] input = userInput();
-        System.out.println("Player Input: " + input[0]);
-        System.out.println("Computer Input: " + input[1]);
 
+        char[] board = creategameBoard();
+
+        System.out.println("Enter index you want to move from 1 to 9");
+        int index = sc.nextInt();
+
+        makeAMove(index, input[0]);
         displayGameBoard();
 
-        makeAMove(2,'x');
+        boolean result = isBoardHaveFreeSpaceOrNot();
+        if(true){
+            System.out.println("Make A Move");
+            makeAMove(index,input[0]);
+        }
+        else{
+            System.out.println("No Free Space!!");
+        }
+
     }
 
     /*Board Created*/
 
-    public static char[] creatgameBoard() {
+    public static char[] creategameBoard() {
         int gameBoardSize = 10;
         char[] gameBoard = new char[gameBoardSize];
         for (int i = 1; i < gameBoardSize; i++) {
@@ -48,18 +57,17 @@ public class TicTacToeGame {
 
     /*Show GameBoard*/
     public static void displayGameBoard() {
-        creatgameBoard();
-        System.out.println(creatgameBoard()[1] + " | " + creatgameBoard()[2] + " | " + creatgameBoard()[3]);
+        System.out.println(creategameBoard()[1] + " | " + creategameBoard()[2] + " | " + creategameBoard()[3]);
         System.out.println("-----------");
-        System.out.println(creatgameBoard()[4] + " | " + creatgameBoard()[5] + " | " + creatgameBoard()[6]);
+        System.out.println(creategameBoard()[4] + " | " + creategameBoard()[5] + " | " + creategameBoard()[6]);
         System.out.println("-----------");
-        System.out.println(creatgameBoard()[7] + " | " + creatgameBoard()[8] + " | " + creatgameBoard()[9]);
+        System.out.println(creategameBoard()[7] + " | " + creategameBoard()[8] + " | " + creategameBoard()[9]);
     }
-    /* Check Index to Make Move if its Empty*/
 
+    /* Check Index to Make Move if its Empty*/
     public static boolean ifIndexEmptyOrNot(int index)
     {
-        if (index > 0 && index <= 9 && creatgameBoard()[index] == ' ')
+        if (index > 0 && index <= 9 && creategameBoard()[index] == ' ')
         {
             return true;
         }
@@ -71,12 +79,21 @@ public class TicTacToeGame {
     /*Make A Move */
     public static void makeAMove(int index,char moveSymbol){
         if(ifIndexEmptyOrNot(index)){
-            creatgameBoard()[index] = moveSymbol;
+            creategameBoard()[index] = moveSymbol;
         }else {
             System.out.println("The index is Already Occupied.");
             System.out.println("Enter new Position");
             int newPosition = sc.nextInt();
             makeAMove(newPosition,moveSymbol);
         }
+    }
+    /*Check if Board Have Free Space*/
+
+    public static boolean isBoardHaveFreeSpaceOrNot() {
+        boolean flag = false;
+        for (int i = 1; i < 10; i++)
+            if (creategameBoard()[i] == ' ')
+                flag = true;
+        return flag;
     }
 }
